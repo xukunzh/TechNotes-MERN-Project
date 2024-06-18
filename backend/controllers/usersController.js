2980a79a6449aa9b6f6a9c0edb2b53bfa270975c
xukunzh <zhang.xuku@northeastern.edu>
2024-06-17 19:17:10 -0700

    debug JWT

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -7,10 +7,12 @@ const bcrypt = require('bcrypt')
 // @route GET /users
 // @access Private
 const getAllUsers = asyncHandler(async (req, res) => {
+    // Get all users from MongoDB
     const users = await User.find().select('-password').lean()
 
+    // If no users 
     if (!users?.length) {
-        return res.status(400).json({ message: 'No users found'})
+        return res.status(400).json({ message: 'No users found' })
     }
 
     res.json(users)
@@ -24,28 +26,28 @@ const createNewUser = asyncHandler(async (req, res) => {
 
     // Confirm data
     if (!username || !password || !Array.isArray(roles) || !roles.length) {
-        return res.status(400).json({ message: 'All fields are required'})
+        return res.status(400).json({ message: 'All fields are required' })
     }
-    
-    // Check for duplicate
+
+    // Check for duplicate username
     const duplicate = await User.findOne({ username }).lean().exec()
 
     if (duplicate) {
-        return res.status(409).json({ message: 'Duplicate username'})
+        return res.status(409).json({ message: 'Duplicate username' })
     }
 
-    // Hash password
-    const hashedPWD = await bcrypt.hash(password, 10)
-    
-    const userObiject = { username, "password" : hashedPWD, roles }
-    
+    // Hash password 
+    const hashedPwd = await bcrypt.hash(password, 10) // salt rounds
+
+    const userObject = { username, "password": hashedPwd, roles }
+
     // Create and store new user 
-    const user = await User.create(userObiject)
-    
-    if (user) {
+    const user = await User.create(userObject)
+
+    if (user) { //created 
         res.status(201).json({ message: `New user ${username} created` })
     } else {
-        res.status(400).json({ message: 'Invalid user created'})
+        res.status(400).json({ message: 'Invalid user data received' })
     }
 })
 
@@ -53,8 +55,8 @@ const createNewUser = asyncHandler(async (req, res) => {
 // @route PATCH /users
 // @access Private
 const updateUser = asyncHandler(async (req, res) => {
-    const { id, username, password, roles, active } = req.body
-    
+    const { id, username, roles, active, password } = req.body
+
     // Confirm data 
     if (!id || !username || !Array.isArray(roles) || !roles.length || typeof active !== 'boolean') {
         return res.status(400).json({ message: 'All fields except password are required' })
@@ -80,9 +82,10 @@ const updateUser = asyncHandler(async (req, res) => {
     user.active = active
 
     if (password) {
-        user.password = await bcrypt.hash(password, 10)
+        // Hash password 
+        user.password = await bcrypt.hash(password, 10) // salt rounds 
     }
-    
+
     const updatedUser = await user.save()
 
     res.json({ message: `${updatedUser.username} updated` })
